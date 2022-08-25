ba08e6e4eab6c9f724fff3bab0ee787449e31f14
premj44 <premjadhav904445>
2022-08-26 04:12:17 +0530

    added day3 work

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,13 +1,14 @@
 import {Route, Routes} from "react-router-dom"
-import National from "../Components/National"
+import National from "../Routes/National"
 import Navbar from "../Components/Navbar"
+import Home from "./Home"
 
 export default function AllRoutes(){
 
     return(
         <div>
         <Routes>
-        <Route path="/" element={<Navbar/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/national" element={<National/>} />
          {/* <Route path="/" element={<h1>home</h1>}/> */}
         </Routes>
